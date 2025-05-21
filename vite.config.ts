59d8c06e2d32a85f12b6c922d5f912ef99db140e
Helmut Schreier <6533040+HSchreier@users.noreply.github.com>
2025-05-21 23:54:09 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glsl'],
+  base: '/SFTC-Playground/' // Replace with your repository name
 })
